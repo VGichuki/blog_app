57bf38091d958309f2eaa90d237ac021c3ec0c45
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-06-18 22:17:28 +0300

    create a route for the quotes in the views.py file call it in the index.html file

--- app/views.py
+++ app/views.py
@@ -1,8 +1,12 @@
 from flask import render_template
 from app import app
+from .request import get_quotes
 
 #Views
 @app.route("/")
 def index():
-
-    return render_template("index.html")
+    '''
+    View root page function that returns the index page and its data
+    '''
+    quotes = get_quotes()
+    return render_template("index.html", quotes = quotes)
