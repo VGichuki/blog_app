bdca19b8fc4007ec3fbdac201489927762788fa7
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-06-18 22:18:13 +0300

    install bootstrap and initialise it in the __init__.py file and link it in the base.html file

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
-#initializing the application
+# Initializing the application
 app = Flask(__name__,instance_relative_config=True)
 
-#setting up the configuration
+# Setting up the configuration
 app.config.from_object(DevConfig)
 app.config.from_pyfile("config.py")
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
 
 from app import views
