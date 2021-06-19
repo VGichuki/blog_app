30fe12851b309756987a383fd666a68c1ba3976c
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-06-19 23:51:37 +0300

    create update.html in profile folder to eneble a user to input a profile pic

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 
 bootstrap = Bootstrap()
@@ -12,6 +13,8 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
+photos = UploadSet('photos', IMAGES)
+
 def create_app(config_name):
     
     # Initializing the application
@@ -20,6 +23,9 @@ def create_app(config_name):
     # Setting up the configuration
     app.config.from_object(config_options[config_name])
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # Configure UploadSet
+    configure_uploads(app, photos)
 
     # Initializing Flask Extensions
     bootstrap.init_app(app)
